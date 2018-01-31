5958b34a6b51fa787c262963a0f0dc8809050d87
Stefan Keil <an.stefan.keil@googlemail.com>
2018-01-31 12:45:40 +0100

    refactor(Draggable) clean up draggable creation

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import turfBearing from '@turf/bearing';
 import turfDistance from '@turf/distance';
 import turfDestination from '@turf/destination';
 import { coordAll as turfCoordAll } from '@turf/meta';
-import turfUnion from '@turf/union';
-import turf from '@turf/helpers';
 
 let id = 0;
 
@@ -29,6 +27,7 @@ L.TrueSize = L.Layer.extend({
   initialize(geoJSON = this.geoJSON, options = {}) {
     // merge default and passed options
     this._options = L.Util.extend(this.options, options);
+
     this._geometryType = geoJSON.geometry.type;
 
     L.Util.setOptions(this, this._options);
@@ -48,41 +47,49 @@ L.TrueSize = L.Layer.extend({
     // our currentlayer is always the first layer of geoJson layersgroup
     // but has a dynamic key
     const currentLayer = this._geoJSONLayer._layers[Object.keys(this._geoJSONLayer._layers)[0]];
-    this._makeDraggable(currentLayer);
 
-    this._options.markerDiv.length && this._addMarker(currentLayer, this._options);
+    this._draggableLayer = this._createDraggable(currentLayer);
+
+    if (this._options.markerDiv.length) {
+      this._dragMarker = this._createMarker(currentLayer, this._options);
+      this._dragMarker.addTo(this._map);
+    }
   },
 
-  _addMarker(layer, options) {
+  _createMarker(layer, options) {
     const { markerClass, markerDiv } = options;
     const dragIcon = L.divIcon({ className: markerClass, html: markerDiv });
-    this._dragMarker = L.marker(layer.getCenter(), { icon: dragIcon, draggable: true })
-      .on('drag', evt => this._onDrag(evt, layer))
-      .addTo(this._map);
+    const dragMarker = L.marker(layer.getCenter(), { icon: dragIcon, draggable: true });
+
+    return this._addHooks(dragMarker, layer);
   },
 
-  _makeDraggable(layer) {
+  _createDraggable(layer) {
     const draggable = new L.Draggable(layer._path);
-    draggable
-      .on('drag', evt => this._onDrag(evt, layer))
-      .enable();
+    draggable.enable();
+
+    return this._addHooks(draggable, layer);
+  },
+
+  _addHooks(item, layer) {
+    return item.on('drag', evt => this._onDrag(evt, layer));
   },
 
   _onDrag(evt, layer) {
-    const newPoint = L.point(evt.originalEvent.clientX, evt.originalEvent.clientY);
-    const latlng = this._map.containerPointToLatLng(newPoint);
+    const { clientX, clientY } = evt.originalEvent;
+    const pointerPos = L.point(clientX, clientY);
+    const { lng, lat } = this._map.containerPointToLatLng(pointerPos);
 
-    const newCenter = [latlng.lng, latlng.lat];
+    const newCenter = [lng, lat];
     const prevBearingDistance = this._getBearingDistance(layer);
     this._redraw(layer, newCenter, prevBearingDistance);
   },
 
   _getBearingDistance(layer) {
-    const center = turfCenter(layer.feature).geometry.coordinates;
-
+    const prevCenter = turfCenter(layer.feature).geometry.coordinates;
     return turfCoordAll(layer.feature).map(coord => {
-      const bearing = turfBearing(center, coord);
-      const distance = turfDistance(center, coord, { units: 'kilometers' });
+      const bearing = turfBearing(prevCenter, coord);
+      const distance = turfDistance(prevCenter, coord, { units: 'kilometers' });
       return { bearing, distance };
     });
   },
@@ -109,13 +116,18 @@ L.TrueSize = L.Layer.extend({
 
     // add draggable hook again, as we using internal a new layer
     // center marker if existing
-    this._makeDraggable(currentLayer);
+    this._draggableLayer = this._createDraggable(currentLayer);
     this._dragMarker && this._dragMarker.setLatLng(currentLayer.getCenter());
   },
 
   onRemove(map) {
     this._map = map;
     this._map.removeLayer(this._geoJSONLayer);
+    if (this._dragMarker) {
+      this._map.removeLayer(this._dragMarker);
+    }
+
+    return this;
   },
 
   _getCoordsByType(point, type) {
