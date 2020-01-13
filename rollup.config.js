026030fc2e0fd18c0db471ec2d6699978e43db8b
moklick <info@moritzklack.com>
2020-01-13 12:52:41 +0100

    refactor(rollup): update deps and cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,51 +1,73 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import replace from 'rollup-plugin-replace';
 import babel from 'rollup-plugin-babel';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
 import html from 'rollup-plugin-html';
 
+import pkg from './package.json';
+
 const isProd = process.env.NODE_ENV === 'production';
 
-export default {
-  name: 'Leaflet.TrueSize',
-  input: 'src/index.js',
-  output: {
-    file: 'build/Leaflet.TrueSize.js',
-    format: 'umd'
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      file: pkg.browser,
+      format: 'umd',
+      globals: {
+        leaflet: 'L'
+      },
+      name: 'Leaflet.TrueSize',
+      sourcemap: true
+    },
+    external: ['leaflet'],
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel({
+        presets: ['@babel/preset-env'],
+        exclude: 'node_modules/**'
+      }),
+      !isProd &&
+        livereload({
+          watch: ['docs', 'playground', 'dist']
+        }),
+      !isProd &&
+        serve({
+          port: 3000,
+          contentBase: ['playground', 'docs', 'dist']
+        }),
+      !isProd &&
+        html({
+          include: 'playground/index.html'
+        })
+    ]
   },
-  sourcemap: true,
-  external: [
-    'leaflet',
-  ],
-  globals: {
-    'leaflet': 'L'
-  },
-  plugins: [
-    resolve(),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-    commonjs({
-      include: ['node_modules/@turf/**']
-    }),
-    babel({
-      presets: [
-        ["es2015", {"modules": false}]
-      ],
-      plugins: ["external-helpers"],
-      exclude: 'node_modules/**'
-    }),
-    !isProd && livereload({
-      watch: ['docs', 'playground', 'build']
-    }),
-    !isProd && serve({
-      port: 3000,
-      contentBase: ['playground', 'docs', 'build']
-    }),
-    !isProd && html({
-      include: 'playground/index.html'
-    })
-  ]
-};
+  {
+    input: 'src/index.js',
+    external: [
+      'leaflet',
+      '@turf/bearing',
+      '@turf/distance',
+      '@turf/destination',
+      '@turf/meta'
+    ],
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        globals: {
+          leaflet: 'L'
+        }
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+        globals: {
+          leaflet: 'L'
+        }
+      }
+    ]
+  }
+];
--- rollup.config.prod.js
+++
@@ -1,7 +0,0 @@
-import butternut from 'rollup-plugin-butternut';
-import config from './rollup.config';
-
-config.output.file = 'build/Leaflet.TrueSize.min.js';
-config.plugins.push(butternut());
-
-export default config;
